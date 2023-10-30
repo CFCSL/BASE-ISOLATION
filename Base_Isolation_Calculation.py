a6d6fc800b91631e0ede35a8762dec13bef83db6
NamNguyen2015 <111955400+NamNguyen2015@users.noreply.github.com>
2023-10-30 18:01:23 +0100

    update page

--- Base_Isolation_Calculation.py
+++ Base_Isolation_Calculation.py
@@ -42,7 +42,7 @@ def scientific_format(x):
 
 
 #%%
-def Iteration(m,n,n_c,W_SS, W_PP,W,K_sub,angle_skew,PGA, S_1,S_S, SiteClass,q,k,tol,T_max, Isolator_Type,latex_format=True,plot_action=False, d=2):
+def B1(m,n,n_c,W_SS, W_PP,W,K_sub,angle_skew,PGA, S_1,S_S, SiteClass,T_max, Isolator_Type,tol,latex_format=True,plot_action=False, d=2):
 	
 	"""
 	m: Number of supports
@@ -88,7 +88,7 @@ def Iteration(m,n,n_c,W_SS, W_PP,W,K_sub,angle_skew,PGA, S_1,S_S, SiteClass,q,k,
 	# Call the Response Spectrum function
 	C_sm, F_pga, F_a, F_v, A_S, S_DS,S_D1=AASHTO(t, PGA,S_S,S_1,SiteClass) 
 	
-
+	print(f'S_D1={S_D1}')
 	 # Plot the design response spectrum
 	
 	if plot_action==True:
@@ -116,32 +116,49 @@ def Iteration(m,n,n_c,W_SS, W_PP,W,K_sub,angle_skew,PGA, S_1,S_S, SiteClass,q,k,
 	i=1
 	
 	#d=10*S_D1
-	#d=1.84
+	d0=2.0
 	data=dict()
 
+
+	
+	#print(f'iteration:{i} ')
+	#print(f'd={d}')
+	
+	##%% Calculate characteristic strength, Q_d
+	Q_d=0.05*W_SS
+
+	##%% Calculate Post-yield stiffness, K_d
+	K_d=0.05*(W_SS/d0)
+
+	### B2.1.2.1.2—Step B1.2: Initial Isolator Properties at Supports
+
+	##%% Calculate the characteristic strength, Q_dj
+
+	Q_dj=[Q_d*(W[j]/W_SS) for j in range(m)]
+
+	##%% Calculate postelastic stiffness, K_dj
+
+	K_dj= [K_d*(W[j]/W_SS) for j in range(m)]
+
 	while True:
-		
-		#print(f'iteration:{i} ')
-		#print(f'd={d}')
-		
-		##%% Calculate characteristic strength, Q_d
-		Q_d=q*W_SS
-
-		##%% Calculate Post-yield stiffness, K_d
-		K_d=k*(W_SS/d)
-
-		### B2.1.2.1.2—Step B1.2: Initial Isolator Properties at Supports
-
-		##%% Calculate the characteristic strength, Q_dj
-
-		Q_dj=[Q_d*(W[j]/W_SS) for j in range(m)]
-
-		##%% Calculate postelastic stiffness, K_dj
-
-		K_dj= [K_d*(W[j]/W_SS) for j in range(m)]
-
-
-		### B2.1.2.1.3—Step B1.3: Effective Stiffness of Combined Pier and Isolator System
+# =============================================================================
+# 		##%% Calculate characteristic strength, Q_d
+# 		Q_d=0.05*W_SS
+# 
+# 		##%% Calculate Post-yield stiffness, K_d
+# 		K_d=0.05*(W_SS/d)
+# 
+# 		### B2.1.2.1.2—Step B1.2: Initial Isolator Properties at Supports
+# 
+# 		##%% Calculate the characteristic strength, Q_dj
+# 
+# 		Q_dj=[Q_d*(W[j]/W_SS) for j in range(m)]
+# 
+# 		##%% Calculate postelastic stiffness, K_dj
+# 
+# 		K_dj= [K_d*(W[j]/W_SS) for j in range(m)]
+# 		### B2.1.2.1.3—Step B1.3: Effective Stiffness of Combined Pier and Isolator System
+# =============================================================================
 
 		##%% Calculate alpha_j
 
@@ -162,7 +179,7 @@ def Iteration(m,n,n_c,W_SS, W_PP,W,K_sub,angle_skew,PGA, S_1,S_S, SiteClass,q,k,
 
 		##%% Calculate the displacement of the isolation system, d_isolj
 
-		d_isolj=  [d/(1+ alpha_j[j]) for j in range(m)]
+		d_isolj= [d/(1+ alpha_j[j]) for j in range(m)]
 		
 		#print(f'd_isolj: {d_isolj}')
 
@@ -230,8 +247,8 @@ def Iteration(m,n,n_c,W_SS, W_PP,W,K_sub,angle_skew,PGA, S_1,S_S, SiteClass,q,k,
 							"$$d_{isol,j}$$":d_isolj,
 							"$$K_{isol,j}$$": K_isolj,
 							"$$d_{sub,j}$$": d_subj, 
-							"$$F_{sub,j}$$":F_subj ,
-							"$$ F_{col,j,k}$$":F_coljk,
+							"$$F_{sub,j}$$":F_subj,
+							"$$F_{col,j,k}$$":F_coljk,
 							"$$T_{eff}$$": T_eff,
 							"$$K_{eff}$$":K_eff,
 							"$$\\xi$$":xi,
@@ -248,13 +265,13 @@ def Iteration(m,n,n_c,W_SS, W_PP,W,K_sub,angle_skew,PGA, S_1,S_S, SiteClass,q,k,
 		##%%%%%%%%%%%%%%%%%%%%%
 		##%% Calculate the diference, abs(d_new-d) 
 
-		difference=abs((d_new-d)/d)
+		difference=abs(d_new-d)
 		#delta=difference/d
 
 		##%% Check the convergence condition:
 
 		#if difference> epsilon:
-		if difference> tol:
+		if difference>= tol:
 			d=d_new
 			i+=1
 
@@ -291,7 +308,7 @@ def Iteration(m,n,n_c,W_SS, W_PP,W,K_sub,angle_skew,PGA, S_1,S_S, SiteClass,q,k,
 				"$$K_{isol,j}$$": "K_isolj",
 				"$$d_{sub,j}$$": "d_subj",
 				"$$F_{sub,j}$$": "F_subj",
-				"$$ F_{col,j,k}$$": "F_coljk",
+				"$$F_{col,j,k}$$": "F_coljk",
 				"$$T_{eff}$$": "T_eff",
 				"$$K_{eff}$$": "K_eff",
 				"$$\\xi$$": "xi",
@@ -299,16 +316,16 @@ def Iteration(m,n,n_c,W_SS, W_PP,W,K_sub,angle_skew,PGA, S_1,S_S, SiteClass,q,k,
 				})
 	 # Concatenate the DataFrames from each iteration
 	concat_df=pd.DataFrame()
-   
+
 	for i, df in data.items():
 		concat_df = pd.concat([concat_df, df], ignore_index=False)
 	#display(list(data.values())[0])
 	#display(list(data.values())[-1])
-	return list(data.values())[-1]
+	return list(data.values())
 
 ###############################################################################
     
-def Multimode(m,n,n_c,W_SS, W_PP,W,K_sub,angle_skew,PGA, S_1,S_S, SiteClass,q,k,tol,T_max, Isolator_Type,latex_format=True):
+def B2(m,n,n_c,W_SS, W_PP,W,K_sub,angle_skew,PGA, S_1,S_S, SiteClass,T_max, Isolator_Type,tol,latex_format=True):
 	
 	#isolator_type = ["friction-based isolators", 'others'] 
 	
@@ -348,6 +365,43 @@ def Multimode(m,n,n_c,W_SS, W_PP,W,K_sub,angle_skew,PGA, S_1,S_S, SiteClass,q,k,
  
 	"""
 
+
+	
+
+
+## Step B1: Iteration
+	df0=B1(m,n,n_c,W_SS, W_PP,W,K_sub,angle_skew,PGA, S_1,S_S, SiteClass,T_max, Isolator_Type,tol,latex_format=False,plot_action=True, d=2.0)
+	display(data)
+
+	d=df0.d.unique()[0]
+	d_new=df0.d_new.unique()[0]
+	print(type(d))
+	Q_d=df0.Q_d.unique()
+	K_d=df0.K_d.unique()
+	alpha_j=df0.alpha_j.to_list()
+	K_effj=df0.K_effj.to_list()
+	d_isolj=df0.d_isolj.to_list()
+	Q_dj=df0.Q_dj.to_list()
+	#print(Q_dj)
+	K_dj=df0.K_dj.to_list()
+	#print(K_dj)
+	K_isolj=df0.K_isolj. to_list()
+	#print(K_isolj)
+	T_eff=df0.T_eff.unique()
+	K_eff=df0.K_eff.unique()
+	xi=df0.xi.unique()
+	B_L=df0.B_L.unique()
+	d_subj=df0.d_subj.to_list()
+
+
+
+## Step B2: Multimode  
+	"""
+	- The results from the Simplified Method (Step B1 in Article B2.1.2.1)
+	are used to determine initial values for the equivalent spring elements for the isolators as a starting point in the
+	iterative process. 
+   
+	"""
 # Calculate Response Spectrum:
 	# Create a array of time:
 	shape=200
@@ -355,42 +409,6 @@ def Multimode(m,n,n_c,W_SS, W_PP,W,K_sub,angle_skew,PGA, S_1,S_S, SiteClass,q,k,
 	# Call the Response Spectrum function
 	C_sm, F_pga, F_a, F_v, A_S, S_DS,S_D1=AASHTO(t, PGA,S_S,S_1,SiteClass) 
 	
-
-
-## Step 1: Iteration
-	data=Iteration(m,n,n_c,W_SS, W_PP,W,K_sub,angle_skew,PGA, S_1,S_S, SiteClass,q,k,tol,T_max, Isolator_Type,latex_format=False,plot_action=True, d=2.0)
-	display(data)
-
-	d=data.d.unique()[0]
-	d_new=data.d_new.unique()[0]
-	print(type(d))
-	Q_d=data.Q_d.unique()
-	K_d=data.K_d.unique()
-	alpha_j=data.alpha_j.to_list()
-	K_effj=data.K_effj.to_list()
-	d_isolj=data.d_isolj.to_list()
-	Q_dj=data.Q_dj.to_list()
-	#print(Q_dj)
-	K_dj=data.K_dj.to_list()
-	#print(K_dj)
-	K_isolj=data.K_isolj. to_list()
-	#print(K_isolj)
-	T_eff=data.T_eff.unique()
-	K_eff=data.K_eff.unique()
-	xi=data.xi.unique()
-	B_L=data.B_L.unique()
-	d_subj=data.d_subj.to_list()
-
-
-
-## Step 2: Multimode  
-	"""
-	- The results from the Simplified Method (Step B1 in Article B2.1.2.1)
-	are used to determine initial values for the equivalent spring elements for the isolators as a starting point in the
-	iterative process. 
-   
-	"""
-
 	N=int((0.8*T_eff*shape)/T_max)
 		
 	C_sm[N:]=C_sm[N:]/B_L # C_sm is calculated by Response Spectrum, and B_L is the result from the last  convergence step.
@@ -402,113 +420,89 @@ def Multimode(m,n,n_c,W_SS, W_PP,W,K_sub,angle_skew,PGA, S_1,S_S, SiteClass,q,k,
 	plt.show()
 	
 	### Begin iteration
-	iter_multimode=1
-	d=d
+	#iter_multimode=1
 	
-# =============================================================================
-# 	print(f'd_multimode {round(d_multimode,2)}')
-# 	
-# 	data_multimode={}
-# 		
-# 	
-# 	
-# =============================================================================
-	while True:
-		## B2.1.2.2.1—Step B2.1:Characteristic Strength
-	
-		##%% Calculate the characteristic strength, Qd,i, and postelastic stiffness, Kd,i, of each isolator “i” 
-		
-		Q_di=[Q_dj[j]/n for j in range(m)]
-		
-		
-		K_di=[K_dj[j]/n for j in range(m)]
-		
-		#print(f'K_di={K_di}')
-	
-		## B2.1.2.2.2—Step B2.2: Initial Stiffness and Yield Displacement
-	
-		##%% Calculate the initial stiffness, Ku,i, and the yield displacement, dy,i, for each isolator “i”
-	
-		if Isolator_Type == "friction-based isolators":
-			K_ui =[np.inf for j in range(m)] # n=6, is the numbers of isolator per each support 
-			d_yi=[0]*m
-		else:
-			K_ui=[10*K_di[j] for j in range(m)]
-			d_yi=[Q_di[j]/(K_ui[j]-K_di[j]) for j in range(m)]
-		#print(f'K_ui={K_ui}')
-		#print(f'd_yi={d_yi}')
-	
-		## B2.1.2.2.3—Step B2.3: Isolator Effective Stiffness, Kisol,i
-	
-		##%% Calculate the isolator stiffness, Kisol,i, of each isolator “i”
-		
-		
-		k_isoli=[K_isolj[j]/n for j in range (m)] #(sample: n=6, m=4)
-		
-		#print(f'k_isoli={k_isoli}')
-	
-		## B2.1.2.2.4—Step B2.4: ThreeDimensional Bridge Model
-	
-		## B2.1.2.2.5—Step B2.5: Composite Design Response Spectrum
-	   
-	
-		## B2.1.2.2.6—Step B2.6: Multimode Analysis of Finite Element Model
-		
-		#( Call the d_isolj from the previuos calculation)
-	
-	
-	
-	
-	
-		##%% Recalculate system damping ratio, ξ :
-		
-		df=Iteration(m,n,n_c,W_SS, W_PP,W,K_sub,angle_skew,PGA, S_1,S_S, SiteClass,q,k,tol,T_max, Isolator_Type,latex_format=False,plot_action=False, d=d_new)
-		d_isoli=df.d_isolj.to_list()
-		print(f'd_isoli={d_isoli}')
-		d_new=df.d_new.unique()[0]
-		d_subj=df.d_subj.to_list()
-		
-		print(d_subj)
 
-		tol=abs((d_new-d)/d)
-		
-		if tol<=0.02:
-			break
-		else:
-			print('continue iteration')
-			
-				
-				
+	## B2.1.2.2.1—Step B2.1:Characteristic Strength
+
+	##%% Calculate the characteristic strength, Qd,i, and postelastic stiffness, Kd,i, of each isolator “i” 
+	
+	Q_di=[Q_dj[j]/n for j in range(m)]
+	
+	
+	K_di=[K_dj[j]/n for j in range(m)]
+	
+	#print(f'K_di={K_di}')
+
+	## B2.1.2.2.2—Step B2.2: Initial Stiffness and Yield Displacement
+
+	##%% Calculate the initial stiffness, Ku,i, and the yield displacement, dy,i, for each isolator “i”
+
+	if Isolator_Type == "friction-based isolators":
+		K_ui =[np.inf for j in range(m)] # n=6, is the numbers of isolator per each support 
+		d_yi=[0]*m
+	else:
+		K_ui=[10*K_di[j] for j in range(m)]
+		d_yi=[Q_di[j]/(K_ui[j]-K_di[j]) for j in range(m)]
+	#print(f'K_ui={K_ui}')
+	#print(f'd_yi={d_yi}')
+	
+	## B2.1.2.2.3—Step B2.3: Isolator Effective Stiffness, Kisol,i
+
+	##%% Calculate the isolator stiffness, Kisol,i, of each isolator “i”
+	
+	
+	k_isoli=[K_isolj[j]/n for j in range (m)] #(sample: n=6, m=4)
+	
+	print(f'k_isoli={k_isoli}')
+
+	## B2.1.2.2.4—Step B2.4: ThreeDimensional Bridge Model
+
+	## B2.1.2.2.5—Step B2.5: Composite Design Response Spectrum
+   
+
+	## B2.1.2.2.6—Step B2.6: Multimode Analysis of Finite Element Model
+	
+	#( Call the d_isolj from the previuos calculation)
+
+	
+	
+	d_est=d
+	d_est_new=d_new
+	delta= abs(d_est-d_est_new)/d_est
+	if delta<=0.05:
+		print('The problm obtains the convergence')
+		df1=B1(m,n,n_c,W_SS, W_PP,W,K_sub,angle_skew,PGA, S_1,S_S, SiteClass,T_max, Isolator_Type,tol,latex_format=False,plot_action=False, d=d_est_new)
+		d_isoli=df1.d_isolj.to_list()
+	else:
+		i_multi=1
+		df1=B1(m,n,n_c,W_SS, W_PP,W,K_sub,angle_skew,PGA, S_1,S_S, SiteClass,T_max, Isolator_Type,tol,latex_format=False,plot_action=False, d=d_est_new)
+		d_est=df1.d.unique()[0]
+		d_est_new=df1.d_new.unique()[0]
+		d_isoli=df1.d_isolj.to_list()
+		while True:
 			## B2.1.2.2.8—Step B2.8: Update Kisol,i, Keff,j, ξ , and BL
 		
 			##%% Use the calculated displacements in each isolator element to obtain new values of Kisol,i for each isolator
 		
 			K_isoli=[Q_di[j]/d_isoli[j]+K_di[j] for j in range(m)] # (d_isoli in the document) is d_isolj in the convergence step
 			
-			#print(f'K_isoli={K_isoli}')
+			print(f'K_isoli={K_isoli}')
 			###%% Recalculate K_effj
 		
 			K_effj=[(K_sub[j]*K_isoli[j]*n)/(K_sub[j]+K_isoli[j]*n) for j in range(m)]
 			
-			#print(f'K_effj={K_effj}')
-		
-		
+			print(f'K_effj={K_effj}')
+
 	
-			
+			##%% Calculate alpha_j
 		
+			#alpha_i = [(K_dj[i]*d1+Q_dj[i])/(K_sub[i]*d1-Q_dj[i]) for i in range(m)]
 		
+			##%% Calculate the displacement of the isolation system, d_isolj
 		
-	# =============================================================================
-	# 	
-	# 	##%% Calculate alpha_j
-	# 
-	# 	alpha_i = [(K_dj[i]*d_multimode+Q_dj[i])/(K_sub[i]*d_multimode-Q_dj[i]) for i in range(m)]
-	# 
-	# 	##%% Calculate the displacement of the isolation system, d_isolj
-	# 
-	# 	d_isoli=  [d_multimode/(1+ alpha_i[i]) for i in range(m)]
-	# 
-	# =============================================================================
+			#d_isoli=  [d1/(1+ alpha_i[i]) for i in range(m)]
+
 			#print(f'd_isoli={d_isoli}')
 			numerator=2*sum([n*Q_di[j]*(d_isoli[j]-d_yi[j]) for j in range(m)])
 			denominator=np.pi*sum([K_effj[j]*(d_isoli[j]+d_subj[j])**2 for j in range(m)]) 
@@ -518,36 +512,22 @@ def Multimode(m,n,n_c,W_SS, W_PP,W,K_sub,angle_skew,PGA, S_1,S_S, SiteClass,q,k,
 			
 			## Recalculate system damping factor, BL:
 			B_L=Piecewise(((xi/0.05)**0.3,xi<0.3),(1.7, xi>=0.3))
-			#print(f'B_L={B_L}')
-			#Recalculate Keff
-		
-		# =============================================================================
-		# 	K_eff=sum(K_effj)
-		# 	
-		# 	#print(f'K_eff={K_eff}')
-		# 	
-		# 	#print(f'W_eff {W_eff}')
-		# 	
-		# 	#print(f'g={g}')
-		# 
-		# 	# Recalculate T_eff
-		# =============================================================================
-			W_eff= W_SS + W_PP #  Effective weight, W_eff
-			g=386.4 # (in./s^2) or 9.815(m/s^2)
-			T_eff= 2*np.pi*(W_eff/(g*K_eff))**(1/2)
+			df2=B1(m,n,n_c,W_SS, W_PP,W,K_sub,angle_skew,PGA, S_1,S_S, SiteClass,T_max, Isolator_Type,tol,latex_format=False,plot_action=False, d=d_est_new)
+			d_isoli_new= df2.d_isolj.to_list()
+			d_est_new=df2.d_new.unique()[0]
 			
-			#print(f'T_eff={T_eff}')
-		
-			## Recalculate d
-			d_multimode_new=(9.79*S_D1*T_eff)/B_L
-			#print(f'd_multimode_new={d_multimode_new}')
-	
-			d=d_multimode_new
-			iter_multimode+=1
+			if abs(d_isoli_new[0]-d_isoli[0])/d_isoli_new[0]<=0.05:
+				break
+			else:
+				i_multi+=1
+				d_isoli=d_isoli_new
+				d_est=d_est_new
 
-	print(f'd_multimode_new {round(d_multimode_new,2)}')
 
-	print(f'Numbers of iteratations to get convergence check: {iter_multimode}')
+
+	print(f'd_multimode_new {round(d_est,2)}')
+
+	print(f'Numbers of iteratations to get convergence check: {i_multi}')
 	
 	
 	#superstructure displacements in the longitudinal (xL) and transverse (yL) directions are:
