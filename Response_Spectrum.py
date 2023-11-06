4d8b1dd5d9f95a836c273f26a343497c7c9b4a5c
NamNguyen2015 <111955400+NamNguyen2015@users.noreply.github.com>
2023-11-06 11:51:55 +0100

    Update Response_Spectrum.py

--- Response_Spectrum.py
+++ Response_Spectrum.py
@@ -10,8 +10,8 @@ Created on Fri May 12 13:40:50 2023
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from ipywidgets import interact, interactive, fixed, interact_manual
-import ipywidgets as widgets
+#from ipywidgets import interact, interactive, fixed, interact_manual
+#import ipywidgets as widgets
 
 
 #%% EuroCode8
