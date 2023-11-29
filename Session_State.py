d63fa00beb3ef87abd1c7c760f0421c4d9520926
NamNguyen2015 <111955400+NamNguyen2015@users.noreply.github.com>
2023-11-29 17:24:46 +0100

    clear button

--- Session_State.py
+++ Session_State.py
@@ -5,68 +5,50 @@ Created on Wed Nov 29 09:25:19 2023
 @author: cfcpc2
 """
 import streamlit as st
-import pandas as pd
 
-def Func_SessionState(*params):
-	a, b = params
-	c = a * b
-	return c
+def Func_SessionState(a, b):
+	if a is not None and b is not None:
+		c = a * b
+		return c
+	else:
+		return None
 
 
-p1 = [1, 2]
-p2 = [4, 5]
+
+p1 = [1.0, 2.0]
+p2 = [4.0, 5.0]
+default=[None, None]
+
 
 samples = {
-	'sample1_0': p1,
-	'sample1_1': p2
+	"manual input": default,
+		'sample1_0': p1,
+		'sample1_1': p2
+	
 }
 
 selected_key = st.radio("**SAMPLES:**", options=list(samples.keys()), index=0, key="select_sample")
-
 st.write(selected_key)
 
+if st.button("Clear Parameters"):
+	selected_key=list(samples.keys())[0]
+	a = st.number_input("a=", format="%.2f", value= samples[selected_key][0])
+	b = st.number_input("b=", format="%.2f", value= samples[selected_key][1])
+else:
+	# Display input fields for parameters
+	a = st.number_input("a=", format="%.2f", value= samples[selected_key][0])
+	b = st.number_input("b=", format="%.2f", value= samples[selected_key][1])
+
+# Create a button to clear input parameters
 
 
-a= st.number_input("a=", format="%.2f",value=samples[selected_key][0] )
-b= st.number_input("b=", format="%.2f",value=samples[selected_key][1] )
-
-#c=Func_SessionState(samples[selected_key][0],samples[selected_key][1])
-c=Func_SessionState(a,b)
-st.write(f" the value is {c}")
-
-
-
-
-
-
-
-
-
-
+if st.button("GENERATE", key="GENERATE"):
+	
+	c = Func_SessionState(a, b)
+	st.write(f"The value is {c}")
 
 
 
 
 
-# =============================================================================
-# 
-# selected_key = st.sidebar.radio("**SAMPLES:**", options=list(samples.keys()), index=0, key="select_sample")
-# if selected_key in samples:
-# 	params = samples[selected_key]
-# 	params_dict = {
-# 		'a': params[0],
-# 		'b': params[1]
-# 	}
-# 
-# 	# Convert the scalar values into a list to create a DataFrame
-# 	df = pd.DataFrame([params_dict])
-# 
-# 	df=st.data_editor(df)
-# 	p=[]
-# 	for k in df:
-# 		p.append(df[k])
-# 	
-# 	c1 = Func_SessionState(p)
-# 	st.write(f" the value is {c1[0]}")
-# =============================================================================
 
